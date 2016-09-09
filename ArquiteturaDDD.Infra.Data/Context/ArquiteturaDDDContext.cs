bf12c291dc6b606c6f89b010bccc786ba3442805
Giovanni Bayerlein de Camargo <re034183@resource.com.br>
2016-09-09 16:49:42 -0300

    Projeto

---
+++ ArquiteturaDDD.Infra.Data/Context/ArquiteturaDDDContext.cs
@@ -0,0 +1,64 @@
+﻿using ArquiteturaDDD.Domain.Entities;
+using ArquiteturaDDD.Infra.Data.EntityConfig;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquiteturaDDD.Infra.Data.Context
+{
+    public class ArquiteturaDDDContext : DbContext
+    {
+        public ArquiteturaDDDContext()
+            : base("ArquiteturaDDD")//String de conexao
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();//REMOVE PLURALIZACAO
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();//REMOVE O DELETE EM CASCADA 1 -> n
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();//REMOVE O DELETE EM CASCADA n -> n
+
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());//Configura os id, ex: ClienteId é um id válido
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));//Configura as strings como varchar
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));//Configura um padrao de 100
+
+
+            modelBuilder.Configurations.Add(new ClienteConfiguration());//Adiciona as configuraçoes extras da entidade cliente
+            modelBuilder.Configurations.Add(new ProdutoConfiguration());//Adiciona as configuraçoes extras da entidade produto
+        }
+
+        public override int SaveChanges()//Sobreescreve o SaveChanges
+        {
+
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return base.SaveChanges();
+        }
+    }
+}
---
+++ ArquiteturaDDD.Infra.Data/EntityConfig/ClienteConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using ArquiteturaDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquiteturaDDD.Infra.Data.EntityConfig
+{
+    //Configuração da entidade cliente
+    public class ClienteConfiguration : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteConfiguration()
+        {
+            HasKey(c => c.ClienteId);//Define o id do cliente
+
+            Property(c => c.Nome)
+                .IsRequired()
+                .HasMaxLength(150);//Configura algumas coisas da propriedade
+
+            Property(c => c.Sobrenome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(c => c.Email)
+                .IsRequired();
+        }
+    }
+}
